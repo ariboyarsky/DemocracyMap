38cc9aec96635db6b34f9a9f1a24acb4b2e36661
Ari Boyarsky <ariboyarsky@gwmail.gwu.edu>
2015-09-21 00:29:07 -0400

    Fix typo in tooltip.json

--- tooltip.js
+++ tooltip.js
@@ -3,7 +3,7 @@ d3.helper = {};
 d3.helper.tooltip = function(access){
 	return function(selection){
 		var tooltipDiv;
-		var bodyNode = s3.select('body').node();
+		var bodyNode = d3.select('body').node();
 		selection.on("click", function(d, i) {
             //remove old tooltip
             d3.select('body').selectAll('div.tooltip').remove();
