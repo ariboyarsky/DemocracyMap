8ca5d340dc208c06de49a12ef766eab277c5dac7
Ari Boyarsky <ariboyarsky@gwmail.gwu.edu>
2015-09-21 00:25:37 -0400

    Add tooltips to map

--- tooltip.js
+++ tooltip.js
@@ -4,8 +4,25 @@ d3.helper.tooltip = function(access){
 	return function(selection){
 		var tooltipDiv;
 		var bodyNode = s3.select('body').node();
-		selection.on("click", function(d, i){
-		//remove old tooltip
-		d3.select('body').selectAll('div.tooltip').remove();
-		//create and append tooltip
-		tooltipDiv = d3.
+		selection.on("click", function(d, i) {
+            //remove old tooltip
+            d3.select('body').selectAll('div.tooltip').remove();
+            //create and append tooltip
+            tooltipDiv = d3.select('body').append('div').attr('class', 'tooltip');
+
+            var mousePosition = d3.mouse(bodyNode);
+            tooltipDiv.style('left', (mousePosition[1] - 15)+'px')
+                .style('top', (mousePosition[1] -15)+'px')
+                .style('position', 'absolute')
+                .style('z-index', 1001);
+
+            //add text
+            var tooltipText = access(d, i) || '';
+            tooltipDiv.html(tooltipText);
+        })
+            .on("click", function(d, i){
+                tooltipDiv.remove();
+            });
+    };
+};
+
